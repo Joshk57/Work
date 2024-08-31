ff92773c9c51d280a2101b3872d7fda203756c07
Joshua Kim <joshk.5793@gmail.com>
2024-08-31 14:06:31 -0400

    solution to 30

--- workfile.js
+++ workfile.js
@@ -36,10 +36,50 @@ dividend = 10, divisor = 3
 
 
 var findSubstring = function(s, words) {
-    
+    let result = [],
+        pattern = {},
+        wordLength = words[0].length;
+
+    for (const word of words) {
+        pattern[word] = (pattern[word] || 0) + 1;
+    }
+
+    for (let i = 0; i < wordLength; i++) {
+        let back = i,
+            front = back + wordLength,
+            matches = {},
+            count = 0
+
+        while (front <= s.length) {
+            let word = s.slice(front - wordLength, front);
+
+            if (pattern[word]) {
+                matches[word] = (matches[word] ?? 0) + 1;
+                count++;
+
+                while (matches[word] > pattern[word]) {
+                    matches[s.slice(back, back + wordLength)] -= 1;
+                    back += wordLength;
+                    count--;
+                }
+
+                if (count === words.length) {
+                    result.push(back)
+                }                
+            } else {
+                matches = {}
+                count = 0;
+                back = front;
+            }
+
+            front += wordLength;
+        }
+    }
+
+    return result;    
 };
 
 s = "barfoothefoobarman", words = ["foo","bar"]
 // s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
 // s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
-console.log(findSubstring(s, words))
+console.log(findSubstring(s, wordss))
