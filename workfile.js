2a4d4e74c57034731a5979058b5731e2c2be7429
Joshua Kim <joshk.5793@gmail.com>
2024-08-31 14:02:04 -0400

    29 divide two integers

--- workfile.js
+++ workfile.js
@@ -1,1 +1,9 @@
-// commit test
+// 29. Divide two integeres
+
+var divide = function(dividend, divisor) {
+    
+};
+
+dividend = 10, divisor = 3
+// dividend = 7, divisor = -3
+console.log(divide(dividend, divisor))
