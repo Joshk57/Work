4b62e54ac9925b9f0bade567754533704c34d28c
Joshua Kim <joshk.5793@gmail.com>
2024-09-07 12:13:32 -0400

    solved next permutation

--- workfile.js
+++ workfile.js
@@ -87,7 +87,15 @@ s = "barfoothefoobarman", words = ["foo","bar"]
 // 31. Next permutation
 
 var nextPermutation = function(nums) {
-    
+    const n = nums.length
+    let i = n - 2
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--
+    if (i >= 0) {
+        let j = n - 1
+        while (nums[j] <= nums[i]) j--
+        swap(nums, i, j)
+    }
+    reverse(nums, i + 1)    
 };
 
 function swap(nums, i, j) {
