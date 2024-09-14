363fd1760a62de738cec5236a54f0b2a0d1127d5
Joshua Kim <joshk.5793@gmail.com>
2024-09-14 05:59:38 -0400

    solved search insert position

--- workfile.js
+++ workfile.js
@@ -146,7 +146,15 @@ s = "(()"
 
 
 var searchInsert = function(nums, target) {
+    let index = nums.indexOf(target)
+   
+    if(index == -1){
+      nums.push(target)
+       nums.sort((a,b) => a - b)
+       return nums.indexOf(target)
+    } 
     
+    return index;  
 };
 
 nums = [1,3,5,6], target = 5
