a47133ddb0c365a98857cff2ef70d6b8a5744cce
Joshua Kim <joshk.5793@gmail.com>
2024-10-12 13:19:26 -0400

    finished jump game 2

--- workfile.js
+++ workfile.js
@@ -430,7 +430,19 @@ s = "aa", p = "a"
 // 45. Jump Game II
 
 var jump = function(nums) {
-    
+    let near = 0, far = 0, jumps = 0;
+
+    while (far < nums.length - 1) {
+        let farthest = 0;
+        for (let i = near; i <= far; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        near = far + 1;
+        far = farthest;
+        jumps++;
+    }
+
+    return jumps;   
 };
 
 nums = [2,3,1,1,4]
