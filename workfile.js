5892a1a78617c8b9d21774a214f8a30a40872112
Joshua Kim <joshk.5793@gmail.com>
2024-10-19 08:09:44 -0400

    group anagrams

--- workfile.js
+++ workfile.js
@@ -545,4 +545,15 @@ var rotate = function(matrix) {
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
 // matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-console.log(rotate(matrix))
+// console.log(rotate(matrix))
+
+// 49. Group Anagrams
+
+var groupAnagrams = function(strs) {
+ 
+};
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+// strs = [""]
+// strs = ["a"]
+console.log(groupAnagrams(strs))
