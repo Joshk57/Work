b499fd33bb64e18c64e2be1ed7990e7ae8116c66
Joshua Kim <joshk.5793@gmail.com>
2024-10-19 08:08:23 -0400

    solved rotate image

--- workfile.js
+++ workfile.js
@@ -519,7 +519,28 @@ nums = [1,1,2]
 // 48. Rotate Image
 
 var rotate = function(matrix) {
-    
+    const edgeLength = matrix.length;
+
+    let top = 0;
+    let bottom = edgeLength - 1;
+
+    while (top < bottom) {
+        for (let col = 0; col < edgeLength; col++) {
+            let temp = matrix[top][col];
+            matrix[top][col] = matrix[bottom][col];
+            matrix[bottom][col] = temp;
+        }
+        top++;
+        bottom--;
+    }
+
+    for (let row = 0; row < edgeLength; row++) {
+        for (let col = row + 1; col < edgeLength; col++) {
+            let temp = matrix[row][col];
+            matrix[row][col] = matrix[col][row];
+            matrix[col][row] = temp;
+        }
+    }    
 };
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
