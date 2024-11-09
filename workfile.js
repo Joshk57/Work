9808ce52fd18fa6e98c1a53ea43f5d7cb0a6287d
Joshua Kim <joshk.5793@gmail.com>
2024-11-09 11:54:24 -0500

    added for loop and rest of function

--- workfile.js
+++ workfile.js
@@ -737,7 +737,21 @@ var spiralOrder = function(matrix) {
     let y = 0;
     let dx = 1;
     let dy = 0;
-    const res = [];    
+    const res = [];   
+    
+    for (let i = 0; i < rows * cols; i++) {
+        res.push(matrix[y][x]);
+        matrix[y][x] = ".";
+
+        if (!(0 <= x + dx && x + dx < cols && 0 <= y + dy && y + dy < rows) || matrix[y+dy][x+dx] === ".") {
+            [dx, dy] = [-dy, dx];
+        }
+
+        x += dx;
+        y += dy;
+    }
+
+    return res;   
 };
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
