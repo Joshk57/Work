d736f9606a327f7dad0b772a19e58c48c343cc18
Joshua Kim <joshk.5793@gmail.com>
2024-11-16 13:36:23 -0500

    solution to 58

--- workfile.js
+++ workfile.js
@@ -811,7 +811,18 @@ intervals = [[1,3],[6,9]], newInterval = [2,5]
 // 58. Length of Last Word
 
 var lengthOfLastWord = function(s) {
-    
+    let end = s.length - 1;
+
+    while (end >= 0 && s[end] === ' ') {
+        end--;
+    }
+
+    let start = end;
+    while (start >= 0 && s[start] !== ' ') {
+        start--;
+    }
+
+    return end - start;  
 };
 
 s = "Hello World"
