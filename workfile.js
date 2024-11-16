849b2dfc64d53f186add53d1aadd94171b238eae
Joshua Kim <joshk.5793@gmail.com>
2024-11-16 13:33:30 -0500

    inserting intervals

--- workfile.js
+++ workfile.js
@@ -781,4 +781,15 @@ var merge = function(intervals) {
 
 intervals = [[1,3],[2,6],[8,10],[15,18]]
 // intervals = [[1,4],[4,5]]
-console.log(merge(intervals))
+// console.log(merge(intervals))
+
+
+// 57. Insert Interval
+
+var insert = function(intervals, newInterval) {
+    
+};
+
+intervals = [[1,3],[6,9]], newInterval = [2,5]
+// intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+console.log(insert(intervals, newInterval))
