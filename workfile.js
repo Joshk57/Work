8aab9aaa35d2380300b7efd1cf804c3a966479f2
Joshua Kim <joshk.5793@gmail.com>
2024-11-30 14:07:24 -0500

    main function logic

--- workfile.js
+++ workfile.js
@@ -888,7 +888,25 @@ n = 3, k = 3
 // 61. Rotate List
 
 var rotateRight = function(head, k) {
-    
+    let n = length(head)
+
+    if (k % n === 0 || !head) return head
+
+    let node = head
+    k = n > k ? k : k % n
+
+    while (n - k - 1) {
+        node = node.next
+        k++
+    }
+    res = tail = node.next
+
+    node.next = null
+    while (tail.next) {
+        tail = tail.next
+    }
+    tail.next = head
+    return res
 };
 
 var length = function (node) {
