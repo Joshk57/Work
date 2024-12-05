88797b278b468ad194f9a9098317cf7d0cd9a7f9
Joshua Kim <joshk.5793@gmail.com>
2024-12-05 07:39:02 -0500

    solved plus one

--- workfile.js
+++ workfile.js
@@ -1023,7 +1023,17 @@ s = "0"
 // 66. Plus One
 
 var plusOne = function(digits) {
-    
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] + 1 !== 10) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+        if (i === 0) {
+            digits.unshift(1);
+            return digits;
+        }
+    } 
 };
 
 digits = [1,2,3]
