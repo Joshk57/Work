10aa2922d44dd653ce1683666600b1f9d77d1529
Joshua Kim <joshk.5793@gmail.com>
2024-12-10 13:04:54 -0500

    solved by iterating from the end

--- workfile.js
+++ workfile.js
@@ -1045,7 +1045,24 @@ digits = [1,2,3]
 // 67. Add Binary
 
 var addBinary = function(a, b) {
-    
+    let carry = 0;
+    let res = '';
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+        let sum = carry;
+        if (i >= 0) sum += parseInt(a[i]);
+        if (j >= 0) sum += parseInt(b[j]);
+
+        res = (sum % 2) + res;
+        carry = Math.floor(sum / 2);
+
+        i--;
+        j--;
+    }
+
+    return res;   
 };
 
 a = "11", b = "1"
