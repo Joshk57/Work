218f104cea0a30387bb2c87ae9d64354557f7b88
Joshua Kim <joshk.5793@gmail.com>
2025-01-04 12:39:18 -0500

    top and bot logic

--- workfile.js
+++ workfile.js
@@ -1229,7 +1229,23 @@ matrix = [[1,1,1],[1,0,1],[1,1,1]]
 
 
 var searchMatrix = function(matrix, target) {
+    let [rows, cols] = [matrix.length, matrix[0].length];
+    let [top, bot] = [0, rows-1];
     
+    while(top <= bot){
+        let row = Math.floor((top +  bot) / 2);
+        if(target > matrix[row][cols-1]) {
+            top = row + 1;
+        } else if(target < matrix[row][0]) {
+            bot = row - 1; 
+        } else {
+            break;
+        }
+    }
+    
+    if(!(top <= bot)) {
+        return false;
+    }
 };
 
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
