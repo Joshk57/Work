3fb3fa31ab8033e418efb471d4c151b1ff3c4de9
Joshua Kim <joshk.5793@gmail.com>
2025-01-11 13:42:15 -0500

    solved sorting colors

--- workfile.js
+++ workfile.js
@@ -1272,7 +1272,21 @@ matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 // 75. Sort Colors
 
 var sortColors = function(nums) {
-    
+    let count = {0: 0, 1: 0, 2: 0};
+
+    for (let i = 0; i < nums.length; i++) {
+        count[nums[i]]++;
+    }
+
+    let idx = 0;
+    for (let color = 0; color < 3; color++) {
+        let freq = count[color];
+        for (let j = 0; j < freq; j++) {
+            nums[idx] = color;
+            idx++;
+        }
+    }    
+
 };
 
 
